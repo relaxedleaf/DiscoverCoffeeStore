cd7b46412e0e261a51f241715cd80aedbbaef20a
Guanghui Li <liguanghui.cs@gmail.com>
2023-01-10 22:40:50 -0600

    new way to add fonts in a next project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,37 @@
 import '../styles/globals.css';
 
 import type { AppProps } from 'next/app';
+import localFont from '@next/font/local';
 
 const year = new Date().getFullYear();
 
+const ibmPlexSans = localFont({
+	src: [
+		{
+			path: '../public/fonts/IBMPlexSans-Regular.ttf',
+			style: 'normal',
+			weight: '500',
+		},
+		{
+			path: '../public/fonts/IBMPlexSans-SemiBold.ttf',
+			style: 'normal',
+			weight: '600',
+		},
+		{
+			path: '../public/fonts/IBMPlexSans-Bold.ttf',
+			style: 'normal',
+			weight: '700',
+		},
+	],
+});
+
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<>
+		<main className={ibmPlexSans.className}>
 			<Component {...pageProps} />
 			<footer>
 				<p>© {year} Guanghui Li</p>
 			</footer>
-		</>
+		</main>
 	);
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,26 +3,7 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
 	return (
 		<Html lang='en'>
-			<Head>
-				<link
-					rel='preload'
-					href='/fonts/IBMPlexSans-Bold.ttf'
-					as='font'
-					crossOrigin='anonymous'
-				/>
-				<link
-					rel='preload'
-					href='/fonts/IBMPlexSans-Regular.ttf'
-					as='font'
-					crossOrigin='anonymous'
-				/>
-				<link
-					rel='preload'
-					href='/fonts/IBMPlexSans-SemiBold.ttf'
-					as='font'
-					crossOrigin='anonymous'
-				/>
-			</Head>
+			<Head />
 			<body>
 				<Main />
 				<NextScript />
